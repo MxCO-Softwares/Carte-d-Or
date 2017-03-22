e77ef3522b695ae0c5b4cb9fc17e236c2e482e43
EliasBorchani <elias.borchani@gmail.com>
2017-03-22 17:45:30 +0100

    début de la page home

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,15 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 var user = [];
+var restaurant = [
+    {name:"mcDo", img:["https://wallpapers.wallhaven.cc/wallpapers/thumb/small/th-1.jpg"]},
+    {name:"OTacos", img:["https://wallpapers.wallhaven.cc/wallpapers/thumb/small/th-2.jpg"]},
+    {name:"Cacao", img:["https://wallpapers.wallhaven.cc/wallpapers/thumb/small/th-3.jpg"]}
+];
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('home', { title: 'Home' });
+    res.render('home', { title: 'Home', restaurants:restaurant});
 });
 
 router.post('/account-creation', function (req, res, next) {
-    user.push({username:req.body.user, password:req.body.password});
-    res.json(user);
+    var found = false;
+    for(var i=0; i<user.length; i++){
+        if(user[i].username == req.body.user){
+            found = true;
+            break;
+        }
+    }
+    if(found){
+        res.render('signin', { title: 'ERROR' });
+    }else{
+        user.push({username:req.body.user, password:req.body.password});
+        res.json(user);
+    }
 });
 
 router.get('/signin', function (req, res, next) {
@@ -26,7 +42,7 @@ router.post('/signin', function (req, res, next) {
         for(var i=0; i<user.length; i++){
             if(user[i].username == req.body.user){
                 if(user[i].password == req.body.password){
-                    req.session.userId = user.username;
+            req.session.userId = user[i].username;
                     res.render('index', {title:req.session.userId});
                     found = true;
                     break;
@@ -41,4 +57,5 @@ router.post('/signin', function (req, res, next) {
         res.redirect('/');
     }
 });
+
 module.exports = router;
